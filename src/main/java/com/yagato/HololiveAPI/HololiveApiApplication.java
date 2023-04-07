8f4d71198e9060260e3052e6e09c1ffcdb55aa4c
Yagato <Yagatomon@gmail.com>
2023-04-07 17:03:40 -0600

    First commit

---
+++ src/main/java/com/yagato/HololiveAPI/HololiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.yagato.HololiveAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HololiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HololiveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yagato/HololiveAPI/HololiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yagato.HololiveAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HololiveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
