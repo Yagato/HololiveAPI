8fcb1ab41f5bb6d966f83fe91f6b7d6bfa68e74b
Yagato <Yagatomon@gmail.com>
2023-04-09 05:29:33 -0600

    Added the Illustrator entity

--- src/main/java/com/yagato/HololiveAPI/illustrator/Illustrator.java
+++ src/main/java/com/yagato/HololiveAPI/illustrator/Illustrator.java
@@ -1,4 +1,69 @@
 package com.yagato.HololiveAPI.illustrator;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.yagato.HololiveAPI.talent.support_entities.Model;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "illustrators")
 public class Illustrator {
+
+    @Id
+    @SequenceGenerator(name = "illustrators_id_generator", sequenceName = "illustrators_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "illustrators_id_generator", strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "illustrators", cascade = CascadeType.ALL)
+    private List<Model> models;
+
+    public Illustrator() {
+
+    }
+
+    public Illustrator(String name, List<Model> models) {
+        this.name = name;
+        this.models = models;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonIgnore
+    public List<Model> getModels() {
+        return models;
+    }
+
+    @JsonProperty
+    public void setModels(List<Model> models) {
+        this.models = models;
+    }
+
+    @Override
+    public String toString() {
+        return "Illustrator{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", models=" + models +
+                '}';
+    }
 }
--- src/main/java/com/yagato/HololiveAPI/talent/support_entities/Model.java
+++ src/main/java/com/yagato/HololiveAPI/talent/support_entities/Model.java
@@ -2,16 +2,19 @@ package com.yagato.HololiveAPI.talent.support_entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.yagato.HololiveAPI.illustrator.Illustrator;
 import com.yagato.HololiveAPI.talent.Talent;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "models")
 public class Model {
 
     @Id
-    @SequenceGenerator(name = "talents_id_generator", sequenceName = "talents_id_seq", allocationSize = 1)
-    @GeneratedValue(generator = "talents_id_generator", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "models_id_generator", sequenceName = "models_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "models_id_generator", strategy = GenerationType.SEQUENCE)
     @Column(name = "id")
     private Integer id;
 
@@ -25,14 +28,23 @@ public class Model {
     @JoinColumn(name = "talents_id")
     private Talent talent;
 
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "models_illustrators",
+            joinColumns = @JoinColumn(name = "model_id"),
+            inverseJoinColumns = @JoinColumn(name = "illustrator_id")
+    )
+    private List<Illustrator> illustrators;
+
     public Model() {
 
     }
 
-    public Model(String name, String imageURL, Talent talent) {
+    public Model(String name, String imageURL, Talent talent, List<Illustrator> illustrators) {
         this.name = name;
         this.imageURL = imageURL;
         this.talent = talent;
+        this.illustrators = illustrators;
     }
 
     public Integer getId() {
@@ -69,6 +81,14 @@ public class Model {
         this.talent = talent;
     }
 
+    public List<Illustrator> getIllustrators() {
+        return illustrators;
+    }
+
+    public void setIllustrators(List<Illustrator> illustrators) {
+        this.illustrators = illustrators;
+    }
+
     @Override
     public String toString() {
         return "Model{" +
@@ -76,6 +96,7 @@ public class Model {
                 ", name='" + name + '\'' +
                 ", imageURL='" + imageURL + '\'' +
                 ", talent=" + talent +
+                ", illustrators=" + illustrators +
                 '}';
     }
 }
