a3b9c697c848badfe1f052fa0fa6817764db7066
Yagato <Yagatomon@gmail.com>
2023-05-01 01:52:01 -0600

    Test(test/controller): Added tests for GenerationControllerTest

---
+++ src/test/java/com/yagato/HololiveAPI/controller/GenerationControllerTest.java
@@ -0,0 +1,186 @@
+package com.yagato.HololiveAPI.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yagato.HololiveAPI.model.Generation;
+import com.yagato.HololiveAPI.service.GenerationService;
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@TestPropertySource("/application-test.properties")
+@AutoConfigureMockMvc
+@SpringBootTest
+@Transactional
+public class GenerationControllerTest {
+
+    private static MockHttpServletRequest request;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private final Dotenv dotenv = Dotenv.load();
+
+    private final String HOLOLIVE_API_TOKEN = dotenv.get("HOLOLIVE_API_TOKEN");
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private GenerationService generationService;
+
+//    @Autowired
+//    Generation generation;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Value("${sql.script.create.generations}")
+    private String sqlInsertGeneration;
+
+    @Value("${sql.script.delete.generations}")
+    private String sqlDeleteGeneration;
+
+    @BeforeAll
+    public static void setup() {
+        request = new MockHttpServletRequest();
+
+//        request.setParameter("id", "0");
+//        request.setParameter("name", "JP1");
+    }
+
+    @BeforeEach
+    public void setupDatabase() {
+        jdbcTemplate.execute(sqlInsertGeneration);
+    }
+
+    @AfterEach
+    public void setupAfterTransaction() {
+        jdbcTemplate.execute(sqlDeleteGeneration);
+    }
+
+    @DisplayName("Find All")
+    @Test
+    public void findAllTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/generations/all"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(1)));
+    }
+
+    @DisplayName("Find Generation by ID")
+    @Test
+    public void findGenerationById() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/generations/{generationId}", 1))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(1)));
+    }
+
+    @DisplayName("Find Generation by Invalid ID")
+    @Test
+    public void findGenerationByInvalidId() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/generations/{generationId}", 0))
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.httpStatus", is("BAD_REQUEST")));
+    }
+
+    @DisplayName("Add Generation")
+    @Test
+    public void addGeneration() throws Exception {
+        Generation generation = new Generation(0, "JP1", null);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/generations/new")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + HOLOLIVE_API_TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(generation)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name", is("JP1")));
+    }
+
+    @DisplayName("Add Generation No Credentials")
+    @Test
+    public void addGenerationNoCredentials() throws Exception {
+        Generation generation = new Generation(0, "JP1", null);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/generations/new")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(generation)))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @DisplayName("Update Generation")
+    @Test
+    public void updateGeneration() throws Exception {
+        Generation generation = generationService.findByName("JP0");
+        generation.setName("Update");
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/generations/update")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + HOLOLIVE_API_TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(generation)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name", is("Update")));
+    }
+
+    @DisplayName("Update No Credentials")
+    @Test
+    public void updateGenerationNoCredentials() throws Exception {
+        Generation generation = generationService.findByName("JP0");
+        generation.setName("Update");
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/generations/new")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(generation)))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @DisplayName("Delete by ID")
+    @Test
+    public void deleteGenerationById() throws Exception {
+        int id = 1;
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/generations/{generationId}", id)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + HOLOLIVE_API_TOKEN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Deleted Generation with id - " + id));
+    }
+
+    @DisplayName("Delete by Invalid ID")
+    @Test
+    public void deleteGenerationByInvalidId() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/generations/{generationId}", 0)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + HOLOLIVE_API_TOKEN))
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.httpStatus", is("BAD_REQUEST")));
+    }
+
+    @DisplayName("Delete by ID No Credentials")
+    @Test
+    public void deleteGenerationByIdNoCredentials() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/generations/{generationId}", 1))
+                .andExpect(status().is4xxClientError());
+    }
+
+}
